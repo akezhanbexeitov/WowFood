253ecf3ff85e76d5b897055f2db4894ce13a66d0
Akezhan Bexeitov <doolath01@gmail.com>
2022-05-03 15:13:27 +0800

    Downloaded other completed html pages from GitHub

---
+++ admin/index.php
@@ -0,0 +1,39 @@
+<?php
+    include('partials/menu.php');
+?>
+
+    <div class='main-content'>
+        <div class='wrapper'>
+            <h1 class='text-center'>DASHBOARD</h1>
+
+            <div class='boxes'>
+                <div class='box text-center'>
+                    <h1>5</h1>
+                    <br/>
+                    Categories
+                </div>
+
+                <div class='box text-center'>
+                    <h1>5</h1>
+                    <br/>
+                    Categories
+                </div>
+
+                <div class='box text-center'>
+                    <h1>5</h1>
+                    <br/>
+                    Categories
+                </div>
+
+                <div class='box text-center'>
+                    <h1>5</h1>
+                    <br/>
+                    Categories
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php 
+    include('partials/footer.php');
+?>
---
+++ admin/partials/footer.php
@@ -0,0 +1,8 @@
+<footer class='footer'>
+        <div class='wrapper'>
+            <p class='text-center'>2022 All rights reserved. Developed by - <a href="#">Akezhan Bexeitov</a></p>
+        </div>
+    </footer>
+    
+</body>
+</html>
