26a80fc491b11aa07d20a433ef4de3c5171c460d
Akezhan Bexeitov <doolath01@gmail.com>
2022-05-15 15:52:34 +0800

    Styled buttons, tables added to manage pages

--- admin/manage-food.php
+++ admin/manage-food.php
@@ -5,6 +5,60 @@
     <div class='main-content'>
         <div class='wrapper'>
             <h1 class='text-center'>Manage Food</h1>
+            <br/>
+
+            <button class='btn-primary'>Add Food</button>
+            <br/>
+            <br/>
+
+            <table class='tbl-full'>
+                <tr>
+                    <th>S.H.</th>
+                    <th>Full Name</th>
+                    <th>Username </th>
+                    <th>Actions</th>
+                </tr>
+
+                <tr>
+                    <td>1. </td>
+                    <td>Akezhan Bexeitov</td>
+                    <td>akidulatini</td>
+                    <td>
+                        <button class='btn-secondary'>Update Admin</button>
+                        <button class='btn-danger'>Delete Admin</button>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>2. </td>
+                    <td>Akezhan Bexeitov</td>
+                    <td>akidulatini</td>
+                    <td>
+                        <button class='btn-secondary'>Update Admin</button>
+                        <button class='btn-danger'>Delete Admin</button>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>3. </td>
+                    <td>Akezhan Bexeitov</td>
+                    <td>akidulatini</td>
+                    <td>
+                        <button class='btn-secondary'>Update Admin</button>
+                        <button class='btn-danger'>Delete Admin</button>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>4. </td>
+                    <td>Akezhan Bexeitov</td>
+                    <td>akidulatini</td>
+                    <td>
+                        <button class='btn-secondary'>Update Admin</button>
+                        <button class='btn-danger'>Delete Admin</button>
+                    </td>
+                </tr>
+            </table>
         </div>
     </div>
 
--- admin/manage-order.php
+++ admin/manage-order.php
@@ -4,7 +4,56 @@
 
     <div class='main-content'>
         <div class='wrapper'>
-            <h1 class='text-center'>Manage Order</h1>
+            <h1>Manage Order</h1>
+
+            <table class='tbl-full'>
+                <tr>
+                    <th>S.H.</th>
+                    <th>Full Name</th>
+                    <th>Username </th>
+                    <th>Actions</th>
+                </tr>
+
+                <tr>
+                    <td>1. </td>
+                    <td>Akezhan Bexeitov</td>
+                    <td>akidulatini</td>
+                    <td>
+                        <button class='btn-secondary'>Update Admin</button>
+                        <button class='btn-danger'>Delete Admin</button>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>2. </td>
+                    <td>Akezhan Bexeitov</td>
+                    <td>akidulatini</td>
+                    <td>
+                        <button class='btn-secondary'>Update Admin</button>
+                        <button class='btn-danger'>Delete Admin</button>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>3. </td>
+                    <td>Akezhan Bexeitov</td>
+                    <td>akidulatini</td>
+                    <td>
+                        <button class='btn-secondary'>Update Admin</button>
+                        <button class='btn-danger'>Delete Admin</button>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>4. </td>
+                    <td>Akezhan Bexeitov</td>
+                    <td>akidulatini</td>
+                    <td>
+                        <button class='btn-secondary'>Update Admin</button>
+                        <button class='btn-danger'>Delete Admin</button>
+                    </td>
+                </tr>
+            </table>
         </div>
     </div>
 
